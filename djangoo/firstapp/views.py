df43fb84e2436e7e3b1e6abc0e42b9f877f9cfa2
@Flynn <flynn.filip.steppe@vub.be>
2021-11-29 15:47:18 +0100

    Removed the old Django framework

--- djangoo/firstapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def sayhello(request): 
-    return render(request, template_name='hello.html')
