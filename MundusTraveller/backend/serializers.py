67170b14a515fb00fcae13a31ee32127d25d9cb5
Endless101 <taoufikcherroud@gmail.com>
2021-12-15 21:44:36 +0100

    Added a serializer
    Added a serializer for reviews and updating reviews

--- MundusTraveller/backend/serializers.py
+++ MundusTraveller/backend/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import CreateUserModel, Login
+from .models import CreateUserModel, Login, ReviewModel, UpdateReviewModel
 
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +13,16 @@ class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = Login
         fields = ('email', 'password')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewModel
+        fields = ('review','rating','country')
+
+
+
+class UpdateReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UpdateReviewModel
+        fields = ('oldContent', 'newContent',)
