4e5b1ddc8724b1fd7ce00d3af4104ffd61f53133
Endless101 <taoufikcherroud@gmail.com>
2021-12-17 16:23:40 +0100

    Added like model to admin page

--- MundusTraveller/backend/admin.py
+++ MundusTraveller/backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CreateUserModel, ReviewModel
+from .models import CreateUserModel, LikeModel, ReviewModel
 
 # Register your models here.
 admin.site.register(CreateUserModel)
 admin.site.register(ReviewModel)
+admin.site.register(LikeModel)
