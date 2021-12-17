2631dc383632d6ea53bbf435e2024647d208ed4c
Endless101 <taoufikcherroud@gmail.com>
2021-12-17 16:24:29 +0100

    Added Like model and modified existing models

--- MundusTraveller/backend/models.py
+++ MundusTraveller/backend/models.py
@@ -4,6 +4,7 @@ from django.db.models.fields import EmailField
 class CreateUserModel(models.Model):
     firstname = models.CharField(max_length=200, default=None)
     lastname = models.CharField(max_length=200, default=None)
+    username = models.CharField(max_length=200)
     password = models.CharField(max_length=200, default=None)
     confirmPassword = models.CharField(max_length=200)
     email = models.EmailField(default=None)
@@ -21,8 +22,14 @@ class ReviewModel(models.Model):
     review = models.CharField(max_length=500)
     rating = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
+    likes = models.IntegerField(default=0)
 
 
 class UpdateReviewModel(models.Model):
     oldContent = models.CharField(max_length=500)
     newContent = models.CharField(max_length=500)
+
+class LikeModel(models.Model):
+    liker = models.CharField(max_length=200)
+    writer = models.CharField(max_length=200)
+    review = models.CharField(max_length=500)
