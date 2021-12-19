199410390d6057dbc80420133cab188024524b85
Endless101 <taoufikcherroud@gmail.com>
2021-12-19 14:02:38 +0100

    Added server side validation to Login

--- MundusTraveller/frontend/src/components/pages/Login.js
+++ MundusTraveller/frontend/src/components/pages/Login.js
@@ -1,17 +1,37 @@
 import React,{Component} from "react"
+import axios from "axios"
 
 
 
 export default class Login extends Component {
     constructor(props) {
         super(props)
+        this.checkEmail = this.checkEmail.bind(this)
+        this.state ={
+            errors : [{}]
+        }
     }
 
-
+    checkEmail(event){
+        const params = new URLSearchParams
+        params.append('email', event.target.value)
+        axios.get("backend/login", {params: params}, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(res => {
+            this.setState({
+                errors : new Array(res.data)
+            })
+            console.log(this.state)
+        })
+    }
+    
 
     render() {
         return (<div><form method="post" action="/backend/login">
-            <label id="email">Email: <input name="email" type="email" required></input></label><br></br>
+            <label id="email">Email: <input name="email" type="email" required onBlur={this.checkEmail}></input></label> {this.state.errors[0].email} <br></br>
             <label id="password">Password: <input name="password" type="password" required></input></label><br></br>
             <input type="submit" value="login"></input>
         </form>
@@ -20,4 +40,4 @@ export default class Login extends Component {
         
         )
     }
-}
+} 
